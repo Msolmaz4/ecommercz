767d94de91400f36ca9b9cd5f3cea829a9b48ba6
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-04-16 13:49:39 +0200

    Nvabarbotsrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React from 'react'
+
+import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
 import './App.css';
+import Header from './componnet/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+      <Header/>
     </div>
   );
 }
