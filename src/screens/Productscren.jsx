8657e1639f211db883b06d941e2664b5496bd5a9
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-04-17 04:07:51 +0200

    useparams

--- src/screens/Productscren.jsx
+++ src/screens/Productscren.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 import data from '../data'
-const Productscren = ({match}) => {
 
-    const productId =match.params.id
+const Productscren = () => {
+
+    const {id} =useParams();
+
+    const elemen =data.find(elemen => elemen.id == id)
+ 
+    
   return (
     <div>Productscren
 
 
-        <h1> product id = {productId}</h1>
+        <h1> product id ={id} </h1>
+        
+        <h1>{elemen.name}</h1>
     </div>
   )
 }
